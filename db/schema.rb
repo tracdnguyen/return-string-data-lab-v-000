8547e0e05f45901d0177d3fd24fb39759d0f447c
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-21 21:30:48 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181121205412) do
+ActiveRecord::Schema.define(version: 20181121212932) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20181121205412) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
-    t.string   "inventory"
+    t.integer  "inventory"
   end
 
 end
