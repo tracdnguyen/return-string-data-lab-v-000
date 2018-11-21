df9741bcbcbce1f481ff98f129ddef0dc9810a77
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-21 21:22:27 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,13 @@ class ProductsController < ApplicationController
   end
 
   def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
   end
 end
