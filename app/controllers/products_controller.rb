6e32fc3b05e62ef5fc402e91bdab8b9b3ae99569
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-21 21:26:51 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,10 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false
+  end
   private
 
   def product_params
